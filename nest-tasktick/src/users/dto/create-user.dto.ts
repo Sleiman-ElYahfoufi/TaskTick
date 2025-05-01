084757e3a3cc2ca0d4c8d70d09379d41a0f1d71b
SleiYah <shy300602@gmail.com>
2025-05-02 00:22:32 +0300

    CreateUserDto: Adding data transfer object with validation for user creation UpdateUserDto: Adding data transfer object with validation for user updates

--- nest-tasktick/src/users/dto/create-user.dto.ts
+++ nest-tasktick/src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+// create-user.dto.ts
+import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ExperienceLevel, UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  @IsNotEmpty()
+  email: string;
+
+  @IsEnum(UserRole, { message: 'Role must be a valid user role' })
+  role?: UserRole;
+
+  @IsEnum(ExperienceLevel, { message: 'Experience level must be valid' })
+  experience_level?: ExperienceLevel = ExperienceLevel.INTERMEDIATE;
+}
--- nest-tasktick/src/users/dto/update-user.dto.ts
+++ nest-tasktick/src/users/dto/update-user.dto.ts
@@ -1,4 +1,28 @@
+// update-user.dto.ts
+import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ExperienceLevel, UserRole } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password?: string;
+
+  @IsOptional()
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(UserRole, { message: 'Role must be a valid user role' })
+  role?: UserRole;
+
+  @IsOptional()
+  @IsEnum(ExperienceLevel, { message: 'Experience level must be valid' })
+  experience_level?: ExperienceLevel;
+}
