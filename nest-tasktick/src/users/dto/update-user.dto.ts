2ed3083726ba92c712d167ed80a70e4ecd2f7160
SleiYah <shy300602@gmail.com>
2025-05-02 16:18:59 +0300

    UpdateUserDto: Cleaning up unnecessary comments and code

--- nest-tasktick/src/users/dto/update-user.dto.ts
+++ nest-tasktick/src/users/dto/update-user.dto.ts
@@ -2,9 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  // No need to redeclare properties - they're all inherited from CreateUserDto
-  // but made optional automatically by PartialType
-  
-  // You could add custom validation here if needed for specific update scenarios
-  // But it's not necessary for basic functionality
 }
