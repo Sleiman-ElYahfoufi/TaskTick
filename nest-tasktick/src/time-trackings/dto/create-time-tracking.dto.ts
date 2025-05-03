76fb8afca0b5c2584f75a374d589f383603d0e49
SleiYah <shy300602@gmail.com>
2025-05-04 01:35:32 +0300

    CreateTimeTrackingDto: Adding data transfer object with validation rules

--- nest-tasktick/src/time-trackings/dto/create-time-tracking.dto.ts
+++ nest-tasktick/src/time-trackings/dto/create-time-tracking.dto.ts
@@ -1,1 +1,27 @@
-export class CreateTimeTrackingDto {}
+import { IsDateString, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateTimeTrackingDto {
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  task_id: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  start_time: Date;
+
+  @IsOptional()
+  @IsDateString()
+  end_time?: Date;
+
+  @IsOptional()
+  @IsNumber()
+  session_duration?: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: Date;
+}
