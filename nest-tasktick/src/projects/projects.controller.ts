e977e8588debdb80843846041cd8f18ebfe7594c
SleiYah <shy300602@gmail.com>
2025-05-03 19:07:17 +0300

    ProjectsController: Adding AuthGuard, HttpCode, and query parameters for filtering

--- nest-tasktick/src/projects/projects.controller.ts
+++ nest-tasktick/src/projects/projects.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, Query, UseGuards } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { AuthGuard } from '../auth/auth.guard';
+import { ProjectStatus } from './entities/project.entity';
 
 @Controller('projects')
+@UseGuards(AuthGuard)
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectsService.create(createProjectDto);
   }
 
   @Get()
-  findAll() {
+  findAll(@Query('name') name?: string, @Query('status') status?: ProjectStatus, @Query('userId') userId?: string) {
+    if (name) {
+      return this.projectsService.findByName(name);
+    }
+    
+    if (status) {
+      return this.projectsService.findByStatus(status);
+    }
+    
+    if (userId) {
+      return this.projectsService.findAllByUserId(+userId);
+    }
+    
     return this.projectsService.findAll();
   }
 
@@ -28,6 +44,7 @@ export class ProjectsController {
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
     return this.projectsService.remove(+id);
   }
