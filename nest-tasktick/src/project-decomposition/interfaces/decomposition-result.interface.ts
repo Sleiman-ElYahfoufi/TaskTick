75c9b616c1c528ad28ffbcf7c13b82825207f287
SleiYah <shy300602@gmail.com>
2025-05-09 01:39:03 +0300

    DecompositionResultInterface: Adding interface for decomposition result data structure

---
+++ nest-tasktick/src/project-decomposition/interfaces/decomposition-result.interface.ts
@@ -0,0 +1,9 @@
+import { GeneratedTaskDto } from '../dto/generated-task.dto';
+import { ProjectDetailsDto } from '../dto/project-details.dto';
+
+export interface DecompositionResult {
+  projectDetails?: ProjectDetailsDto;
+  tasks: GeneratedTaskDto[];
+  projectId?: number;
+  saved: boolean;
+}
