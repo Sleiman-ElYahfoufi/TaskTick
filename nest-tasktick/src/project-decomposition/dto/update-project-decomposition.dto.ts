14425a4c8ec42790b74ac784bf09b486d2500733
SleiYah <shy300602@gmail.com>
2025-05-09 01:19:17 +0300

    UpdateProjectDecompositionDto: Removing unused DTO as it's no longer needed.

--- nest-tasktick/src/project-decomposition/dto/update-project-decomposition.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProjectDecompositionDto } from './create-project-decomposition.dto';
-
-export class UpdateProjectDecompositionDto extends PartialType(CreateProjectDecompositionDto) {}
