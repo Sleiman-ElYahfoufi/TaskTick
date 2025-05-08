6ef67151e9ac509ca4ec0f315a3eb72ccf684966
SleiYah <shy300602@gmail.com>
2025-05-09 01:37:18 +0300

    GenerateTasksDto: Adding DTO for generating tasks with project details

---
+++ nest-tasktick/src/project-decomposition/dto/generate-tasks.dto.ts
@@ -0,0 +1,10 @@
+import { IsNumber, IsNotEmpty, IsOptional, Max, Min, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ProjectDetailsDto } from './project-details.dto';
+
+export class GenerateTasksDto {
+  @ValidateNested()
+  @Type(() => ProjectDetailsDto)
+  projectDetails: ProjectDetailsDto;
+
+}
