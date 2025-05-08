72d9047d217112772fbcff808681f57c1dfc4142
SleiYah <shy300602@gmail.com>
2025-05-09 01:27:43 +0300

    GeneratedTaskDto: Adding basic DTO structure with name property and validation.

---
+++ nest-tasktick/src/project-decomposition/dto/generated-task.dto.ts
@@ -0,0 +1,9 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, Max, Min, IsDateString } from 'class-validator';
+import { PriorityLevel } from '../../tasks/entities/task.entity';
+
+export class GeneratedTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+}
