028bee9356245fd8996b7663d2150e974ab2d53c
SleiYah <shy300602@gmail.com>
2025-05-09 16:57:48 +0300

    ProjectDecompositionController: Removing unused CRUD endpoints and adding AuthGuard

--- nest-tasktick/src/project-decomposition/project-decomposition.controller.ts
+++ nest-tasktick/src/project-decomposition/project-decomposition.controller.ts
@@ -1,34 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 import { ProjectDecompositionService } from './project-decomposition.service';
-import { CreateProjectDecompositionDto } from './dto/create-project-decomposition.dto';
-import { UpdateProjectDecompositionDto } from './dto/update-project-decomposition.dto';
+import { GenerateTasksDto } from './dto/generate-tasks.dto';
+import { DecompositionResult } from './interfaces/decomposition-result.interface';
 
 @Controller('project-decomposition')
+@UseGuards(AuthGuard)
 export class ProjectDecompositionController {
   constructor(private readonly projectDecompositionService: ProjectDecompositionService) {}
 
-  @Post()
-  create(@Body() createProjectDecompositionDto: CreateProjectDecompositionDto) {
-    return this.projectDecompositionService.create(createProjectDecompositionDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.projectDecompositionService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectDecompositionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDecompositionDto: UpdateProjectDecompositionDto) {
-    return this.projectDecompositionService.update(+id, updateProjectDecompositionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectDecompositionService.remove(+id);
-  }
 }
