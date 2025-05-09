5d5077614b7d45f8f32271582c2eabc78390ac56
SleiYah <shy300602@gmail.com>
2025-05-10 01:01:42 +0300

    AiInsightGeneratorService: Adding basic service structure and necessary imports

--- nest-tasktick/src/ai-insights/ai-insight-generator/ai-insight-generator.service.ts
+++ nest-tasktick/src/ai-insights/ai-insight-generator/ai-insight-generator.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ChatOpenAI } from '@langchain/openai';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AiInsight, InsightType } from '../entities/ai-insight.entity';
+import { UsersService } from '../../users/users.service';
+import { TimeTrackingsService } from '../../time-trackings/time-trackings.service';
+import { ProjectsService } from '../../projects/projects.service';
 
 @Injectable()
-export class AiInsightGeneratorService {}
+export class AiInsightGeneratorService {
+ 
+}
