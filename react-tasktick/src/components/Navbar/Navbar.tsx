037fae763a050b6ebaad6dce627f021faa8fb9cf
SleiYah <shy300602@gmail.com>
2025-05-11 02:28:33 +0300

    Navbar: Adding basic navbar structure with logo and styling

--- react-tasktick/src/components/Navbar/Navbar.tsx
+++ react-tasktick/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import "./styles.css";
+import TaskTickLogo from "../../assets/Sleiman_ElYahfoufi_TaskTick.png";
 const Navbar: React.FC = () => {
-  return (
-  <>
-  </>
-  );
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="nav-content">
+                    <div className="logo">
+                        <img
+                            src={TaskTickLogo}
+                            alt="TaskTick Logo"
+                            className="logo-img"
+                        />
+                    </div>
+                    
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
