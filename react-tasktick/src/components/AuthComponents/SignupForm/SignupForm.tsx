c7c1632242ff33a7564389cb4536dcaa0822870b
SleiYah <shy300602@gmail.com>
2025-05-11 21:05:48 +0300

    SignupForm: Adding initial SignupForm component structure and basic styling import

---
+++ react-tasktick/src/components/AuthComponents/SignupForm/SignupForm.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { Eye, EyeOff } from 'lucide-react'
+import '../AuthForms.css'
+
+
+
+const SignupForm: React.FC<SignupFormProps> = () => {
+  
+  }
+
+  return (
+    <div className="signup-form">
+      
+    </div>
+  )
+}
+
+export default SignupForm
