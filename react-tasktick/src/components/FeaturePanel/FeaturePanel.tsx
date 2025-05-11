f6b90d10d0162d99144f554b0bb5d0269ba10505
SleiYah <shy300602@gmail.com>
2025-05-11 04:20:49 +0300

    FeaturePanel: Implementing basic structure and content display

--- react-tasktick/src/components/FeaturePanel/FeaturePanel.tsx
+++ react-tasktick/src/components/FeaturePanel/FeaturePanel.tsx
@@ -9,7 +9,11 @@ interface FeaturePanelProps {
 
 const FeaturePanel: React.FC<FeaturePanelProps> = ({ icon, title, description }) => {
     return (
-     <></>
+        <div className="feature-panel">
+            <div className="feature-icon">{icon}</div>
+            <h3 className="feature-title">{title}</h3>
+            <p className="feature-description">{description}</p>
+        </div>
     );
 };
 
