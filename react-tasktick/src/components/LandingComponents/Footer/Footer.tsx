05c20b7117804008acf842a842c17377483a1674
SleiYah <shy300602@gmail.com>
2025-05-11 15:32:48 +0300

    Footer: Adding TaskTick logo and social media icons to the footer

--- react-tasktick/src/components/LandingComponents/Footer/Footer.tsx
+++ react-tasktick/src/components/LandingComponents/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Footer.css";
+import TaskTickLogo from "../../../assets/Sleiman_ElYahfoufi_TaskTick.png";
+import { Facebook, Instagram, Twitter, Youtube } from "lucide-react";
 
 
 
