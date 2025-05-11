fead112d0282734358ff72cf7832e39975748d49
SleiYah <shy300602@gmail.com>
2025-05-11 13:00:14 +0300

    Testimonials: Adding basic testimonials section structure

--- react-tasktick/src/components/Testimonials/Testimonials.tsx
+++ react-tasktick/src/components/Testimonials/Testimonials.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import Slider from "react-slick";
+import Testimonial from "../Testimonial/Testimonial";
 import "./Testimonials.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+interface TestimonialData {
+    rating: number;
+    quote: string;
+    author: string;
+    position: string;
+    company: string;
+    avatar?: string;
+}
 
 const Testimonials: React.FC = () => {
-    return <></>;
+    
+    return (
+        <section id="testimonials" className="testimonials">
+           
+        </section>
+    );
 };
 
 export default Testimonials;
