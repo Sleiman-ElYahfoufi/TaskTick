8b22c169dc0e0011d72e5cbf297ef88218239d45
SleiYah <shy300602@gmail.com>
2025-05-11 12:57:05 +0300

    Testimonial: Adding basic structure and props for testimonial card component

--- react-tasktick/src/components/Testimonial/Testimonial.tsx
+++ react-tasktick/src/components/Testimonial/Testimonial.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "./Testimonial.css";
+import { CircleUser, Star } from "lucide-react";
 
+interface TestimonialProps {
+    rating: number;
+    quote: string;
+    author: string;
+    position: string;
+    company: string;
+    avatar?: string;
+}
 
-const Testimonial: React.FC = () => {
+const Testimonial: React.FC<TestimonialProps> = ({
+    rating,
+    quote,
+    author,
+    position,
+    company,
+    avatar,
+}) => {
+   
+
     return (
-       <>
-       </>
+        <div className="testimonial-card">
+           
+        </div>
     );
 };
 
