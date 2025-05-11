74d0b7b2d0eea38ba1da8daf29561f699dbc597b
SleiYah <shy300602@gmail.com>
2025-05-12 01:11:57 +0300

    Dashboard: Adding initial dashboard page structure with sidebar placeholder

---
+++ react-tasktick/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../../components/DashboardComponents/Sidebar/Sidebar';
+import './Dashboard.css';
+
+const Dashboard: React.FC = () => {
+
+
+  return (
+    <div className="dashboard-container">
+     
+    </div>
+  );
+};
+
+export default Dashboard;
