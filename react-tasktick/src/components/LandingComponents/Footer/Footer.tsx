e2bc49109dfa94b54e2beeefefb07fc9c83f7093
SleiYah <shy300602@gmail.com>
2025-05-11 15:33:40 +0300

    Footer: Adding basic footer structure and container

--- react-tasktick/src/components/LandingComponents/Footer/Footer.tsx
+++ react-tasktick/src/components/LandingComponents/Footer/Footer.tsx
@@ -7,7 +7,11 @@ import { Facebook, Instagram, Twitter, Youtube } from "lucide-react";
 
 const Footer: React.FC = () => {
     return (
-       <></>
+        <footer className="footer">
+            <div className="footer-container">
+                
+            </div>
+        </footer>
     );
 };
 
