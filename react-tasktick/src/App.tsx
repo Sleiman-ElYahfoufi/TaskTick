48c32b6a290f3c0666cae0182f8a65d8edc7a1b7
SleiYah <shy300602@gmail.com>
2025-05-12 01:22:42 +0300

    App: Adding route for the Dashboard page

--- react-tasktick/src/App.tsx
+++ react-tasktick/src/App.tsx
@@ -3,6 +3,7 @@ import Landing from './pages/Landing/Landing'
 import './App.css'
 import Auth from './pages/Auth/Auth'
 import Onboarding from './pages/Onboarding/Onboarding'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
   return (
@@ -11,7 +12,12 @@ function App() {
         <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/onboarding" element={<Onboarding />} />
-
+ <Route path="/dashboard" element={<Dashboard />}>
+    {/* <Route index element={<DashboardHome />} />
+    <Route path="projects" element={<Projects />} />
+    <Route path="tasks" element={<Tasks />} />
+    <Route path="settings" element={<Settings />} /> */}
+  </Route>
       </Routes>
     </Router>
   )
