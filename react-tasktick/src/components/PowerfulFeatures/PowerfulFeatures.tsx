36cf8e3ffd61a80c972ab40041518f91577b0230
SleiYah <shy300602@gmail.com>
2025-05-11 04:06:55 +0300

    PowerfulFeatures: Adding new component for showcasing app features

---
+++ react-tasktick/src/components/PowerfulFeatures/PowerfulFeatures.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PowerfulFeatures.css";
+
+
+
+const PowerfulFeatures: React.FC = () => {
+    return (
+       <></>
+    );
+};
+
+export default PowerfulFeatures;
