9009f14cb2cde75d6a8deeb003e4fe1f85bb964d
SleiYah <shy300602@gmail.com>
2025-05-12 01:18:32 +0300

    Sidebar: Adding link to the projects page in the sidebar

--- react-tasktick/src/components/DashboardComponents/Sidebar/Sidebar.tsx
+++ react-tasktick/src/components/DashboardComponents/Sidebar/Sidebar.tsx
@@ -41,10 +41,24 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, closeSidebar }) => {
             <span className="nav-text">Dashboard</span>
           </Link>
           
-          </nav>
+          <Link 
+            to="/dashboard/projects" 
+            className={`nav-item ${isActive('/dashboard/projects') ? 'active' : ''}`}
+            onClick={closeSidebar}
+          >
+            <span className="nav-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+            </span>
+            <span className="nav-text">Projects</span>
+          </Link>
+          
+         
+        </nav>
       </div>
       
-     
+    
       </div>
     </div>
   );
