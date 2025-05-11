059d6954b30617547090a99a5d1cddfc112fa61e
SleiYah <shy300602@gmail.com>
2025-05-12 01:18:45 +0300

    Sidebar: Adding link to Tasks page in the sidebar navigation

--- react-tasktick/src/components/DashboardComponents/Sidebar/Sidebar.tsx
+++ react-tasktick/src/components/DashboardComponents/Sidebar/Sidebar.tsx
@@ -54,12 +54,25 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, closeSidebar }) => {
             <span className="nav-text">Projects</span>
           </Link>
           
+          <Link 
+            to="/dashboard/tasks" 
+            className={`nav-item ${isActive('/dashboard/tasks') ? 'active' : ''}`}
+            onClick={closeSidebar}
+          >
+            <span className="nav-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M9 11l3 3L22 4"></path>
+                <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+              </svg>
+            </span>
+            <span className="nav-text">Tasks</span>
+          </Link>
+          
          
         </nav>
       </div>
       
     
-      </div>
     </div>
   );
 };
