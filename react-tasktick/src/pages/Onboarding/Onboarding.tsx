ba495764655126d5c270e427ad5906c9ba075c05
SleiYah <shy300602@gmail.com>
2025-05-11 22:31:51 +0300

    Onboarding: Adding initial onboarding page structure and assets

---
+++ react-tasktick/src/pages/Onboarding/Onboarding.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Onboarding.css';
+import TaskTickLogo from '../../assets/Sleiman_ElYahfoufi_TaskTick.png';
+import OnboardingImage from '../../assets/OnboardingImage.png';
+
+
+
+const Onboarding: React.FC = () => {
+ 
+  return (
+    <div className="onboarding-container">
+     
+    </div>
+  );
+};
+
+export default Onboarding;
