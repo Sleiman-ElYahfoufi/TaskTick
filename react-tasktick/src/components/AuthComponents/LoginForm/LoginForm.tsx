64743f19ddca38c7f2c196a3af92c7ee76ee0dc9
SleiYah <shy300602@gmail.com>
2025-05-11 21:13:29 +0300

    LoginForm: Adding basic structure and handleSubmit function

--- react-tasktick/src/components/AuthComponents/LoginForm/LoginForm.tsx
+++ react-tasktick/src/components/AuthComponents/LoginForm/LoginForm.tsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 import { Eye, EyeOff } from 'lucide-react'
 import '../AuthForms.css'
 
+interface LoginFormProps {
+  onSwitchToSignup: () => void
+}
 
+const LoginForm: React.FC<LoginFormProps> = ({ onSwitchToSignup }) => {
+  const [showPassword, setShowPassword] = useState(false)
 
-const LoginForm: React.FC = () => {
- 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
 
   return (
     <div className="login-form">
-      
+     
     </div>
   )
 }
