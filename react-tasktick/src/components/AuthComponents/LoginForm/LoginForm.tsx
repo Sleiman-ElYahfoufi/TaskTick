4ffda033bbc93a4304c1349bf919fd8ecb9be649
SleiYah <shy300602@gmail.com>
2025-05-11 21:13:03 +0300

    LoginForm: Adding initial LoginForm component structure

---
+++ react-tasktick/src/components/AuthComponents/LoginForm/LoginForm.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { Eye, EyeOff } from 'lucide-react'
+import '../AuthForms.css'
+
+
+
+const LoginForm: React.FC = () => {
+ 
+
+  return (
+    <div className="login-form">
+      
+    </div>
+  )
+}
+
+export default LoginForm
