cea29711e782d96ea47308d554849a9e86b00272
SleiYah <shy300602@gmail.com>
2025-05-11 21:06:09 +0300

    SignupForm: Adding prop to handle switch to login form

--- react-tasktick/src/components/AuthComponents/SignupForm/SignupForm.tsx
+++ react-tasktick/src/components/AuthComponents/SignupForm/SignupForm.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { Eye, EyeOff } from 'lucide-react'
 import '../AuthForms.css'
 
+interface SignupFormProps {
+  onSwitchToLogin: () => void
+}
 
-
-const SignupForm: React.FC<SignupFormProps> = () => {
-  
-  }
+const SignupForm: React.FC<SignupFormProps> = ({ onSwitchToLogin }) => {
+ 
 
   return (
     <div className="signup-form">
