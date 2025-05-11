0d5e45f8ece5eae8275f6324e7e00821c4c5ae80
SleiYah <shy300602@gmail.com>
2025-05-11 15:40:58 +0300

    Landing: Updating component paths and adding Footer component

--- react-tasktick/src/pages/Landing/Landing.tsx
+++ react-tasktick/src/pages/Landing/Landing.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import HeroSection from '../../components/HeroSection/HeroSection';
-import './Landing.css';
-import PowerfulFeatures from '../../components/PowerfulFeatures/PowerfulFeatures';
-import HowItWorks from '../../components/HowItWorks/HowItWorks';
-import Testimonials from '../../components/Testimonials/Testimonials';
-import CTASection from '../../components/CTASection/CTASection';
+import React from "react";
+import Navbar from "../../components/LandingComponents/Navbar/Navbar";
+import HeroSection from "../../components/LandingComponents/HeroSection/HeroSection";
+import "./Landing.css";
+import PowerfulFeatures from "../../components/LandingComponents/PowerfulFeatures/PowerfulFeatures";
+import HowItWorks from "../../components/LandingComponents/HowItWorks/HowItWorks";
+import Testimonials from "../../components/LandingComponents/Testimonials/Testimonials";
+import CTASection from "../../components/LandingComponents/CTASection/CTASection";
+import Footer from "../../components/LandingComponents/Footer/Footer";
 
 const Landing: React.FC = () => {
-  return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <PowerfulFeatures />
-      <HowItWorks />
-      <Testimonials />
-      <CTASection />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <HeroSection />
+            <PowerfulFeatures />
+            <HowItWorks />
+            <Testimonials />
+            <CTASection />
+            <Footer />
+        </div>
+    );
 };
 
 export default Landing;
