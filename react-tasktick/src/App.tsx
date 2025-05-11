fe3ecc9800c4bdee2f7f0e97a1af58c25cab26a0
SleiYah <shy300602@gmail.com>
2025-05-11 22:46:24 +0300

    App: Adding route for the onboarding page

--- react-tasktick/src/App.tsx
+++ react-tasktick/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing'
 import './App.css'
 import Auth from './pages/Auth/Auth'
+import Onboarding from './pages/Onboarding/Onboarding'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/onboarding" element={<Onboarding />} />
 
       </Routes>
     </Router>
