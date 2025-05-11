af1d7382ddb78633f0b5ebddfb34c25a4882df59
SleiYah <shy300602@gmail.com>
2025-05-11 22:32:32 +0300

    Onboarding: Adding basic container structure for the onboarding page

--- react-tasktick/src/pages/Onboarding/Onboarding.tsx
+++ react-tasktick/src/pages/Onboarding/Onboarding.tsx
@@ -3,13 +3,16 @@ import './Onboarding.css';
 import TaskTickLogo from '../../assets/Sleiman_ElYahfoufi_TaskTick.png';
 import OnboardingImage from '../../assets/OnboardingImage.png';
 
-
+interface ExperienceLevel {
+  label: string;
+  years: string;
+}
 
 const Onboarding: React.FC = () => {
  
   return (
     <div className="onboarding-container">
-     
+      
     </div>
   );
 };
