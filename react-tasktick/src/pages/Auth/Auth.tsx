88d091bc2db3d989c2c7fb436f4885d01f01e7dc
SleiYah <shy300602@gmail.com>
2025-05-11 20:59:25 +0300

    Auth: Initializing basic Auth page structure with login mode state

--- react-tasktick/src/pages/Auth/Auth.tsx
+++ react-tasktick/src/pages/Auth/Auth.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react'
-
+import LoginForm from '../../components/AuthComponents/LoginForm/LoginForm'
+import SignupForm from '../../components/AuthComponents/SignupForm/SignupForm'
+import './Auth.css'
+import AuthImage from '../../assets/AuthImage.png'
 
 const Auth: React.FC = () => {
-  
+  const [isLogin, setIsLogin] = useState(true)
+
+  const handleSwitchToSignup = () => {
+    setIsLogin(false)
+  }
+
+  const handleSwitchToLogin = () => {
+    setIsLogin(true)
+  }
 
   return (
-<></>
+    <div className={`auth-page ${isLogin ? 'login-mode' : 'signup-mode'}`}>
+     
+    </div>
   )
 }
 
