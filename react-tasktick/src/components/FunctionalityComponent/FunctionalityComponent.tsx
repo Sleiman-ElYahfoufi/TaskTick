1bd6863c92e17565667a094162686f6155ae2776
SleiYah <shy300602@gmail.com>
2025-05-11 04:48:23 +0300

    FunctionalityComponent: Adding props for icon, title, and description

--- react-tasktick/src/components/FunctionalityComponent/FunctionalityComponent.tsx
+++ react-tasktick/src/components/FunctionalityComponent/FunctionalityComponent.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "./FunctionalityComponent.css";
 
+interface FunctionalityComponentProps {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+}
 
-
-const FunctionalityComponent: React.FC = () => {
+const FunctionalityComponent: React.FC<FunctionalityComponentProps> = ({ 
+    icon, 
+    title, 
+    description
+}) => {
     return (
-     <></>
+       <></>
     );
 };
 
