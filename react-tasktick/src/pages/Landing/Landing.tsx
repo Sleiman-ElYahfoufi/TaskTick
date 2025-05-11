3c4d81436e59d5bce7d60900a98d663defb6a5e9
SleiYah <shy300602@gmail.com>
2025-05-11 04:24:34 +0300

    Landing: Adding PowerfulFeatures component to display key features

--- react-tasktick/src/pages/Landing/Landing.tsx
+++ react-tasktick/src/pages/Landing/Landing.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import HeroSection from '../../components/HeroSection/HeroSection';
 import './Landing.css';
+import PowerfulFeatures from '../../components/PowerfulFeatures/PowerfulFeatures';
 
 const Landing: React.FC = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <PowerfulFeatures />
     </div>
   );
 };
