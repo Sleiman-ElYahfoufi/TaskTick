940de3edf79f13144c14fb84abac9e185f1d2ee0
SleiYah <shy300602@gmail.com>
2025-05-11 21:20:03 +0300

    App: Adding route for authentication page

--- react-tasktick/src/App.tsx
+++ react-tasktick/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing'
 import './App.css'
+import Auth from './pages/Auth/Auth'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-  
+          <Route path="/auth" element={<Auth />} />
+
       </Routes>
     </Router>
   )
