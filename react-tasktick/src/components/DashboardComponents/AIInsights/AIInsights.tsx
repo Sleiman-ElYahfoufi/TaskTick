1834a63405878dc36acc7df16bed9ab18cad898b
SleiYah <shy300602@gmail.com>
2025-05-13 01:58:49 +0300

    AIInsights: Adding initial AI insights component structure

---
+++ react-tasktick/src/components/DashboardComponents/AIInsights/AIInsights.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AIInsights.css';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+
+
+
+const AIInsights: React.FC = () => {
+  return (
+    <div className="ai-insights">
+   
+    </div>
+  );
+};
+
+export default AIInsights;
