f11f14d60a6adbd1cb2cd4df9ba81ef7de0bab6b
SleiYah <shy300602@gmail.com>
2025-05-13 02:56:53 +0300

    Projects: Adding project interface and initial state variables

--- react-tasktick/src/pages/Projects/Projects.tsx
+++ react-tasktick/src/pages/Projects/Projects.tsx
@@ -3,8 +3,24 @@ import ProjectCard, { ProjectStatus } from '../../components/ProjectsComponents/
 import ProjectFilters from '../../components/ProjectsComponents/ProjectFilters/ProjectFilters';
 import './Projects.css';
 
+interface Project {
+  id: string;
+  title: string;
+  description: string;
+  status: ProjectStatus;
+  estimatedHours: string;
+  tasksCompleted: number;
+  totalTasks: number;
+  lastUpdatedDate: string;
+  lastUpdatedTime: string;
+}
 
 const Projects: React.FC = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [sortBy, setSortBy] = useState('Last Updated');
 
 
   return (
