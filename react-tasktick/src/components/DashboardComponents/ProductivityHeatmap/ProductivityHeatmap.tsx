311c1fc03dba30174464fd7d6964703503b7f7ee
SleiYah <shy300602@gmail.com>
2025-05-13 01:54:25 +0300

    ProductivityHeatmap: Adding data prop to ProductivityHeatmap component

--- react-tasktick/src/components/DashboardComponents/ProductivityHeatmap/ProductivityHeatmap.tsx
+++ react-tasktick/src/components/DashboardComponents/ProductivityHeatmap/ProductivityHeatmap.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './ProductivityHeatmap.css';
 import { ResponsiveHeatMap } from '@nivo/heatmap';
 
+interface ProductivityHeatmapProps {
+  data?: any[];
+}
 
-
-const ProductivityHeatmap: React.FC = () => {
-  
+const ProductivityHeatmap: React.FC<ProductivityHeatmapProps> = ({ data }) => {
+ 
 
   return (
     <div className="productivity-trends">
-     
+      
     </div>
   );
 };
