47be9352f34662f0b1ad857ea046db4ab75fd516
SleiYah <shy300602@gmail.com>
2025-05-13 01:33:19 +0300

    App: Replacing Dashboard component with DashboardLayout component

--- react-tasktick/src/App.tsx
+++ react-tasktick/src/App.tsx
@@ -1,26 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing/Landing'
-import './App.css'
-import Auth from './pages/Auth/Auth'
-import Onboarding from './pages/Onboarding/Onboarding'
-import Dashboard from './pages/Dashboard/Dashboard'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+import "./App.css";
+import Auth from "./pages/Auth/Auth";
+import Onboarding from "./pages/Onboarding/Onboarding";
+
+import DashboardLayout from "./pages/DashboardLayout/DashboardLayout";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/onboarding" element={<Onboarding />} />
- <Route path="/dashboard" element={<Dashboard />}>
-    {/* <Route index element={<DashboardHome />} />
-    <Route path="projects" element={<Projects />} />
-    <Route path="tasks" element={<Tasks />} />
-    <Route path="settings" element={<Settings />} /> */}
-  </Route>
-      </Routes>
-    </Router>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/onboarding" element={<Onboarding />} />
+                <Route path="/dashboard" element={<DashboardLayout />}>
+                   
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
