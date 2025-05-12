af4c6e570338ab582b1ece2f74bbe9f88166549c
SleiYah <shy300602@gmail.com>
2025-05-13 01:59:29 +0300

    AIInsights: Removing placeholder content and adding insights prop interface

--- react-tasktick/src/components/DashboardComponents/AIInsights/AIInsights.tsx
+++ react-tasktick/src/components/DashboardComponents/AIInsights/AIInsights.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './AIInsights.css';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
+interface Insight {
+  id: string;
+  text: string;
+}
 
+interface AIInsightsProps {
+  insights: Insight[];
+}
 
-
-const AIInsights: React.FC = () => {
+const AIInsights: React.FC<AIInsightsProps> = ({ insights }) => {
   return (
     <div className="ai-insights">
-   
+     
     </div>
   );
 };
