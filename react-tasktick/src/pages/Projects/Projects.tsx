cbae5779cf19df60eff44841b636ac995da28f82
SleiYah <shy300602@gmail.com>
2025-05-13 02:53:55 +0300

    Projects: Adding initial project page structure and basic layout

---
+++ react-tasktick/src/pages/Projects/Projects.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import ProjectCard, { ProjectStatus } from '../../components/ProjectsComponents/ProjectCard/ProjectCard';
+import ProjectFilters from '../../components/ProjectsComponents/ProjectFilters/ProjectFilters';
+import './Projects.css';
+
+
+const Projects: React.FC = () => {
+
+
+  return (
+    <div className="projects-page">
+
+     
+    </div>
+  );
+};
+
+export default Projects;
