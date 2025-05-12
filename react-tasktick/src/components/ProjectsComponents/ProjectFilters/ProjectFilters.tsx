d803633d2b482a7b0fbfceeb073269b65c719556
SleiYah <shy300602@gmail.com>
2025-05-13 02:38:31 +0300

    ProjectFilters: Adding initial component structure and basic container

---
+++ react-tasktick/src/components/ProjectsComponents/ProjectFilters/ProjectFilters.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './ProjectFilters.css';
+
+
+
+const ProjectFilters: React.FC = () => {
+ 
+
+  return (
+    <div className="project-filters-container">
+     
+    </div>
+  );
+};
+
+export default ProjectFilters;
