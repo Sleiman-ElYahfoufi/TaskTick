3c603011d8214b9037035715943704c41f8faff5
SleiYah <shy300602@gmail.com>
2025-05-13 01:42:24 +0300

    StatCards: Adding props for dynamic data display

--- react-tasktick/src/components/DashboardComponents/StatCards/StatCards.tsx
+++ react-tasktick/src/components/DashboardComponents/StatCards/StatCards.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './StatCards.css';
 
+interface StatCardsProps {
+  activeProjects: number;
+  tasksCompleted: number;
+  dueToday: number;
+  completedMonth?: string;
+}
 
-
-const StatCards: React.FC = () => {
+const StatCards: React.FC<StatCardsProps> = ({ 
+  activeProjects, 
+  tasksCompleted, 
+  dueToday, 
+  completedMonth = 'this month' 
+}) => {
   return (
     <div className="stat-cards">
-     
+      
     </div>
   );
 };
