cbafae7c34a6baf0cc58ad0ca9c6d220b64cef1c
SleiYah <shy300602@gmail.com>
2025-05-13 01:41:45 +0300

    StatCards: Adding initial component structure and CSS file

---
+++ react-tasktick/src/components/DashboardComponents/StatCards/StatCards.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './StatCards.css';
+
+
+
+const StatCards: React.FC = () => {
+  return (
+    <div className="stat-cards">
+     
+    </div>
+  );
+};
+
+export default StatCards;
