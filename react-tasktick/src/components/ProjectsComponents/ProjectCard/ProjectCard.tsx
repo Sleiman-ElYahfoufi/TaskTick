48c84d787d153c27f2586e336ac2b99872e0884c
SleiYah <shy300602@gmail.com>
2025-05-13 02:24:15 +0300

    ProjectCard: Adding project card component and basic styling

--- react-tasktick/src/components/ProjectsComponents/ProjectCard/ProjectCard.tsx
+++ react-tasktick/src/components/ProjectsComponents/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import './ProjectCard.css';
 
+export type ProjectStatus = 'in-progress' | 'planning' | 'delayed' | 'completed';
 
+interface ProjectCardProps {
+  id: string;
+  title: string;
+  description: string;
+  status: ProjectStatus;
+  estimatedHours: string;
+  tasksCompleted: number;
+  totalTasks: number;
+  lastUpdatedDate: string;
+  lastUpdatedTime: string;
+}
 
 const ProjectCard: React.FC = () => {
   
