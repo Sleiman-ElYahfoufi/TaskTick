d1c9bb63d8d443bc26b4717fd6fe840a9c2d7a68
SleiYah <shy300602@gmail.com>
2025-05-13 01:49:46 +0300

    ProductivityHeatmap: Adding initial component structure and CSS file

---
+++ react-tasktick/src/components/DashboardComponents/ProductivityHeatmap/ProductivityHeatmap.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ProductivityHeatmap.css';
+import { ResponsiveHeatMap } from '@nivo/heatmap';
+
+
+
+const ProductivityHeatmap: React.FC = () => {
+  
+
+  return (
+    <div className="productivity-trends">
+     
+    </div>
+  );
+};
+
+export default ProductivityHeatmap;
