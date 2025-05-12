f61049f797bf1943d6c2d5817d058f21585f5201
SleiYah <shy300602@gmail.com>
2025-05-13 02:39:11 +0300

    ProjectFilters: Adding type definitions for sort and filter options and props

--- react-tasktick/src/components/ProjectsComponents/ProjectFilters/ProjectFilters.tsx
+++ react-tasktick/src/components/ProjectsComponents/ProjectFilters/ProjectFilters.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import './ProjectFilters.css';
 
+type SortOption = 'Last Updated' | 'Name A-Z' | 'Name Z-A' | 'Oldest First' | 'Newest First';
+type FilterOption = 'All' | 'Active' | 'Completed' | 'Planning';
 
+interface ProjectFiltersProps {
+  onSearch: (searchTerm: string) => void;
+  onFilterChange: (filter: FilterOption) => void;
+  onSortChange: (sortBy: SortOption) => void;
+  onNewProject: () => void;
+}
 
 const ProjectFilters: React.FC = () => {
  
