b10f75f843fa639a26305435c1a8927304dbe836
SleiYah <shy300602@gmail.com>
2025-05-13 03:51:56 +0300

    ProjectOverviewCard: Adding initial component structure and CSS file

---
+++ react-tasktick/src/components/ProjectDetailsComponents/ProjectOverviewCard/ProjectOverviewCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ProjectOverviewCard.css';
+
+
+const ProjectOverviewCard: React.FC = () => {
+  return (
+   <></>
+  );
+};
+
+export default ProjectOverviewCard;
