ebb564fa6dce066052a42f9b82f39c4a14cadede
SleiYah <shy300602@gmail.com>
2025-05-13 03:52:09 +0300

    ProjectOverviewCard: Adding props and default value for compact prop

--- react-tasktick/src/components/ProjectDetailsComponents/ProjectOverviewCard/ProjectOverviewCard.tsx
+++ react-tasktick/src/components/ProjectDetailsComponents/ProjectOverviewCard/ProjectOverviewCard.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './ProjectOverviewCard.css';
 
+interface ProjectOverviewCardProps {
+  name: string;
+  accuracy: number;
+  completedTasks: number;
+  totalTasks: number;
+  timeSpent: number;
+  totalEstimatedTime: number;
+  compact?: boolean;
+}
 
-const ProjectOverviewCard: React.FC = () => {
+const ProjectOverviewCard: React.FC<ProjectOverviewCardProps> = ({
+  name,
+  accuracy,
+  completedTasks,
+  totalTasks,
+  timeSpent,
+  totalEstimatedTime,
+  compact = false
+}) => {
   return (
    <></>
   );
