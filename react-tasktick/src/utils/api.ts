e6044380579b2bd1a96b125e04fce59cc1b55049
SleiYah <shy300602@gmail.com>
2025-05-13 22:01:24 +0300

    Api: Creating Axios instance for API communication

---
+++ react-tasktick/src/utils/api.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosError } from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+});
+
