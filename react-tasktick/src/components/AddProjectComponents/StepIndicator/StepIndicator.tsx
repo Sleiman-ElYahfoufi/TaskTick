d0126fc996f8730f19cb06ab32ab40c70c1c7eb0
SleiYah <shy300602@gmail.com>
2025-05-13 03:08:02 +0300

    StepIndicator: Adding initial component structure and CSS file

---
+++ react-tasktick/src/components/AddProjectComponents/StepIndicator/StepIndicator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './StepIndicator.css';
+
+
+
+const StepIndicator: React.FC = () => {
+  return (
+    <div className="step-indicator">
+      
+    </div>
+  );
+};
+
+export default StepIndicator;
