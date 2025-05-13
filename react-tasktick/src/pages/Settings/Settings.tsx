38b46a2de42a7f34feaeafac575461378752a40a
SleiYah <shy300602@gmail.com>
2025-05-13 04:31:23 +0300

    Settings: Adding initial settings page component with basic structure

---
+++ react-tasktick/src/pages/Settings/Settings.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Settings.css';
+
+
+const Settings: React.FC = () => {
+
+
+
+
+
+  return (
+    <div className="settings-container">
+    </div>
+  );
+};
+
+export default Settings;
