5a18201ba21a7b541d6a62fb005500e9cfd3b22e
SleiYah <shy300602@gmail.com>
2025-05-13 04:15:38 +0300

    TaskFilters: Adding basic component structure and placeholder UI

--- react-tasktick/src/components/TasksComponents/TaskFilters/TaskFilters.tsx
+++ react-tasktick/src/components/TasksComponents/TaskFilters/TaskFilters.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './TaskFilters.css';
 
+interface TaskFiltersProps {
+  searchTerm: string;
+  projectFilter: string;
+  statusFilter: string;
+  dueDateFilter: string;
+  onSearchChange: (value: string) => void;
+  onProjectFilterChange: (value: string) => void;
+  onStatusFilterChange: (value: string) => void;
+  onDueDateFilterChange: (value: string) => void;
+  projectOptions?: string[];
+  statusOptions?: string[];
+  dueDateOptions?: string[];
+}
 
 const TaskFilters: React.FC = () => {
   return (
