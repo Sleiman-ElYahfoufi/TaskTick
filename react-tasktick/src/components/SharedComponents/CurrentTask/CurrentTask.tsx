8d0486bfb52a82d88ac196bd9c61e6bc16ec72bb
SleiYah <shy300602@gmail.com>
2025-05-13 03:56:58 +0300

    CurrentTask: Adding initial component structure and basic styling

---
+++ react-tasktick/src/components/SharedComponents/CurrentTask/CurrentTask.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import StopIcon from '@mui/icons-material/Stop';
+import './CurrentTask.css';
+
+
+const CurrentTask: React.FC = () => {
+ 
+  
+  return (
+    <div className="current-task-section">
+     
+    </div>
+  );
+};
+
+export default CurrentTask;
