262d742238ba7fef11cd7f42972d0280b3896722
SleiYah <shy300602@gmail.com>
2025-05-13 03:08:30 +0300

    StepIndicator: Adding steps and currentStep props to StepIndicator component

--- react-tasktick/src/components/AddProjectComponents/StepIndicator/StepIndicator.tsx
+++ react-tasktick/src/components/AddProjectComponents/StepIndicator/StepIndicator.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './StepIndicator.css';
 
+interface Step {
+  number: number;
+  label: string;
+}
 
+interface StepIndicatorProps {
+  steps: Step[];
+  currentStep: number;
+}
 
-const StepIndicator: React.FC = () => {
+const StepIndicator: React.FC<StepIndicatorProps> = ({ steps, currentStep }) => {
   return (
     <div className="step-indicator">
-      
+     
     </div>
   );
 };
