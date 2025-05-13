def2bf80c86451f0f6f4effc4c3b0a5fa9c71ed6
SleiYah <shy300602@gmail.com>
2025-05-13 04:15:27 +0300

    TaskFilters: Adding initial TaskFilters component structure

---
+++ react-tasktick/src/components/TasksComponents/TaskFilters/TaskFilters.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TaskFilters.css';
+
+
+const TaskFilters: React.FC = () => {
+  return (
+    <div className="task-filters">
+      
+    </div>
+  );
+};
+
+export default TaskFilters;
