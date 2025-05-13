9d867e48f9e59299cc099b5726a0629c21b8ee1f
SleiYah <shy300602@gmail.com>
2025-05-13 03:19:12 +0300

    AddProject: Adding initial component structure and StepIndicator import

---
+++ react-tasktick/src/pages/AddProject/AddProject.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import StepIndicator from '../../components/AddProjectComponents/StepIndicator/StepIndicator';
+
+const AddProject: React.FC = () => {
+  
+ 
+    
+  return (
+    <div className="add-project-container">
+    
+    </div>
+  );
+};
+
+export default AddProject;
