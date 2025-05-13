411ba33242789bfd2672e1b8ac7fd2187685ce84
SleiYah <shy300602@gmail.com>
2025-05-13 03:36:54 +0300

    GeneratedTasksPage: Creating initial file structure and basic component layout

---
+++ react-tasktick/src/pages/GeneratedTasks/GeneratedTasksPage.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { 
+  DataGrid, 
+  GridColDef, 
+  GridRenderCellParams, 
+  GridRowId,
+  GridValueGetterParams,
+  GridActionsCellItem,
+  GridEventListener,
+  GridRowModel
+} from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { v4 as uuidv4 } from 'uuid';
+import StepIndicator from '../../components/AddProjectComponents/StepIndicator/StepIndicator';
+import './GeneratedTasksPage.css';
+
+
+const GeneratedTasksPage: React.FC = () => {
+ 
+
+  
+  
+  
+  
+
+  
+
+  return (
+    <div className="generated-tasks-container">
+      
+    </div>
+  );
+};
+
+export default GeneratedTasksPage;
