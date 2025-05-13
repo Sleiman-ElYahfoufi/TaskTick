8699f8bb38ace710050bdac046eb9f81e7dbcb88
SleiYah <shy300602@gmail.com>
2025-05-14 02:08:58 +0300

    TaskBreakerLoader: Adding initial loader component structure

---
+++ react-tasktick/src/components/Loader/TaskBreakerLoader.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./TaskBreakerLoader.css";
+
+
+
+
+const TaskBreakerLoader: React.FC = () => {
+    
+
+    return (
+        <AnimatePresence>
+            
+        </AnimatePresence>
+    );
+};
+
+export default TaskBreakerLoader;
