b6a271556b54de496d0981b566e4b1c9fa350837
SleiYah <shy300602@gmail.com>
2025-05-14 01:29:10 +0300

    ProjectDecompositionService: Adding ProjectDetailsDto interface for project creation payload

--- react-tasktick/src/services/projectDecompositionService.ts
+++ react-tasktick/src/services/projectDecompositionService.ts
@@ -1,5 +1,13 @@
 import api from '../utils/api';
 
+export interface ProjectDetailsDto {
+    name: string;
+    description: string;
+    priority?: string;
+    detail_depth?: string;
+    deadline?: string;
+}
+
 
 
 class ProjectDecompositionService {
