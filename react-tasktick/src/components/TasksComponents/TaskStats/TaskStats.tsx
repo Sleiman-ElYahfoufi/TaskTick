59c3342e9526dc95a36c7f161e76383c792a3f62
SleiYah <shy300602@gmail.com>
2025-05-13 04:17:51 +0300

    TaskStats: Adding initial TaskStats component structure

---
+++ react-tasktick/src/components/TasksComponents/TaskStats/TaskStats.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TaskStats.css';
+
+
+const TaskStats: React.FC = () => {
+  return (
+    <div className="task-stats">
+      
+    </div>
+  );
+};
+
+export default TaskStats;
