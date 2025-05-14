995960da9b05df924ca24ca0c01b972ad1e8e3ac
SleiYah <shy300602@gmail.com>
2025-05-14 17:27:56 +0300

    ProjectsService: Adding initial service class structure

---
+++ react-tasktick/src/services/projectsService.ts
@@ -0,0 +1,16 @@
+import api from '../utils/api';
+import { ProjectStatus } from '../components/ProjectsComponents/ProjectCard/ProjectCard';
+
+
+
+
+
+
+
+
+
+class ProjectsService {
+   
+}
+
+export default new ProjectsService(); 
