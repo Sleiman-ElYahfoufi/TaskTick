1ceee411ce63ef7db87751f38d09345c36a7bdf4
SleiYah <shy300602@gmail.com>
2025-05-15 00:08:56 +0300

    Tasks: Removing unused renderPriorityCell import

--- react-tasktick/src/pages/Tasks/Tasks.tsx
+++ react-tasktick/src/pages/Tasks/Tasks.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { GridColDef } from "@mui/x-data-grid";
 import TasksTable, {
-    renderPriorityCell,
     renderActionsCell,
     renderStatusCell,
     renderProjectCell,
