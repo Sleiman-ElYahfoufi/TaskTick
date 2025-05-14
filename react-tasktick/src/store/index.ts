8651d5397fb461eda5f00aac269715d1b5f18511
SleiYah <shy300602@gmail.com>
2025-05-14 18:46:39 +0300

    Index: Adding tasks and projects reducers to the store

--- react-tasktick/src/store/index.ts
+++ react-tasktick/src/store/index.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import tasksReducer from './slices/tasksSlice';
+import projectsReducer from './slices/projectsSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        tasks: tasksReducer,
+        projects: projectsReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
