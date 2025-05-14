7d377c6e29a843e4595add6a6c92550bab1b2c9b
SleiYah <shy300602@gmail.com>
2025-05-14 18:39:55 +0300

    TasksSlice: Adding loadingTaskIds, cellUpdateErrors states and selectors for task loading and cell update errors

--- react-tasktick/src/store/slices/tasksSlice.ts
+++ react-tasktick/src/store/slices/tasksSlice.ts
@@ -6,36 +6,43 @@ interface TasksState {
     tasks: ProjectTask[];
     currentTask: ProjectTask | null;
     isLoading: boolean;
+    loadingTaskIds: (string | number)[]; 
     error: string | null;
+    cellUpdateErrors: Record<string, string>; 
 }
 
 const initialState: TasksState = {
     tasks: [],
     currentTask: null,
     isLoading: false,
-    error: null
+    loadingTaskIds: [],
+    error: null,
+    cellUpdateErrors: {}
 };
+
+
 
 export const selectAllTasks = (state: RootState) => state.tasks.tasks;
 export const selectCurrentTask = (state: RootState) => state.tasks.currentTask;
 export const selectTasksLoading = (state: RootState) => state.tasks.isLoading;
 export const selectTasksError = (state: RootState) => state.tasks.error;
+export const selectLoadingTaskIds = (state: RootState) => state.tasks.loadingTaskIds;
+export const selectIsTaskLoading = (taskId: string | number) => (state: RootState) =>
+    state.tasks.loadingTaskIds.includes(taskId);
+export const selectCellUpdateErrors = (state: RootState) => state.tasks.cellUpdateErrors;
 
-const setPending = (state: TasksState) => {
-    state.isLoading = true;
-    state.error = null;
-};
 
-const setFailed = (state: TasksState, action: PayloadAction<any>) => {
-    state.isLoading = false;
-    state.error = action.payload as string;
-};
 
-const tasksSlice = createSlice({
-    name: 'tasks',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {}
-});
 
-export default tasksSlice.reducer;
+export const {
+    selectTask,
+    clearTasksError,
+    clearTasks,
+    optimisticUpdateTask,
+    optimisticUpdateCell,
+    clearCellUpdateError,
+    startTaskLoading,
+    stopTaskLoading
+} = tasksSlice.actions;
+
+export default tasksSlice.reducer; 
