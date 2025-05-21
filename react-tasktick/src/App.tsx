a2e693a3577d4cab6474f89d63ec6476082ca3c4
SleiYah <shy300602@gmail.com>
2025-05-21 06:42:23 +0300

    App: Switching from BrowserRouter to HashRouter for improved routing in environments without server-side configuration

--- react-tasktick/src/App.tsx
+++ react-tasktick/src/App.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
+
 import "./App.css";
 import { Toaster } from "react-hot-toast";
 import AppRoutes from "./components/AppRoutes/AppRoutes";
 
 function App() {
     return (
-        <Router>
+        <HashRouter>
             <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
             <AppRoutes />
-        </Router>
+        </HashRouter>
     );
 }
 
