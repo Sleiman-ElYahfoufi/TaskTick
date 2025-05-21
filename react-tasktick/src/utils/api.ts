3d111b76a2075098dd7dbeee912b03caf4eb9d86
SleiYah <shy300602@gmail.com>
2025-05-21 03:37:14 +0300

    Api: Updating default baseURL to production server IP

--- react-tasktick/src/utils/api.ts
+++ react-tasktick/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL || 'http://13.39.242.149:3000',
     headers: {
         'Content-Type': 'application/json',
     },
